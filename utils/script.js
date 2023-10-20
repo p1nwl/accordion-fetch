d5c4706edf4ee4ab380b56e4c26cd47fd9eae90e
Maxim Semchishin <max.semchishin@gmail.com>
2023-10-20 15:29:31 +0500

    placement changes and code rework

---
+++ utils/script.js
@@ -0,0 +1,56 @@
+export function accordion() {
+    const accordionContainer = document.createElement('div');
+    
+    
+    const getData = (url) => 
+        new Promise((resolve, reject) => 
+            fetch(url)
+            .then(response => response.json())
+            .then(json => resolve(json))
+            .catch(error => reject(error))
+    )
+    
+    getData('https://jsonplaceholder.typicode.com/posts/1/comments')
+        .then(function (data) {   
+            data.forEach(item => createAccordionItem(item));
+            accordionContainer.className = 'accordion-container';
+            document.body.append(accordionContainer) 
+            new Accordion('.accordion-container');
+        })
+        .catch(error => console.log(error.message));
+    
+    function createAccordionItem(item) {
+        const accordion = document.createElement('div');
+        accordion.className = 'ac';
+        accordionContainer.append(accordion)
+    
+        const acHeader = document.createElement('h2');
+        acHeader.className = 'ac-header';
+        accordion.append(acHeader);
+        acHeader.append(item.name)
+    
+        const acTrigger = document.createElement('button');
+        acTrigger.className = 'ac-trigger';
+        acTrigger.type = 'button';
+        acHeader.append(acTrigger);
+    
+        const acPanel = document.createElement('div');
+        acPanel.className = 'ac-panel';
+        accordion.append(acPanel);
+    
+        const acText = document.createElement('p');
+        acText.className = 'ac-text';
+        acPanel.append(acText);
+        
+        const entries = Object.entries(item)
+        for (const entry of entries) {
+            const textItem = document.createElement('div');
+            textItem.className = 'text-item'
+            acText.append(textItem)
+            const key = entry[0];
+            const value = entry[1];
+          
+            textItem.append(`${key}: ${value}`);
+        }
+    }
+}
