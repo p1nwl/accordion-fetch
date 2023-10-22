b2c7493264afbc3c3142ddeb0d6ccaea10f00d91
Maxim Semchishin <max.semchishin@gmail.com>
2023-10-22 10:44:26 +0500

    html and css rework

---
+++ utils/responsive.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelector('.nav-links');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const lines = document.querySelectorAll('.line')
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+  lines.forEach(line => {
+    line.classList.toggle('active')
+  });
+});
